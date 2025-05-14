b228bff999f071618e3479826ffa018cb4eb6a03
Yisaterese <teresejosephyisa@gmail.com>
2025-05-14 12:27:03 +0100

    tryig to fix deployment error

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import HomePage from "./pages/homePage.tsx";
-import Dashboard from "./pages/dashboard.tsx";
+// import Dashboard from "./pages/dashboard.tsx";
 
 export default function App() {
     return (
         <div>
             < HomePage/>
-            <Dashboard/>
+            {/*<Dashboard/>*/}
         </div>
     );
 }
